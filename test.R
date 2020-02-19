aceb24b5cfb37b47c0c811604841e373eff18022
periklis91 <ptsirostsib@gmail.com>
2020-02-19 16:12:13 +0200

    adding a new file

---
+++ test.R
@@ -0,0 +1,10 @@
+
+library(bnlearn)
+gr=empty.graph(LETTERS[1:5]) 
+arc.set=matrix(c("A","B","B","C","B","D","C","E","D","E"),ncol=2,byrow=TRUE,dimnames=list(NULL,c("FROM","TO")))
+e = gr
+arcs(e)=arc.set 
+
+par(mfrow=c(1,2)) 
+plot(gr)
+plot(e)
