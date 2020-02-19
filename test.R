947e185543375e7291c4db14bb04d12a18629f09
VaRoum <60233353+VaRoum@users.noreply.github.com>
2020-02-19 17:01:25 +0200

    Update test.R

--- test.R
+++ test.R
@@ -1,10 +1,12 @@
 
 library(bnlearn)
 gr=empty.graph(LETTERS[1:5]) 
-arc.set=matrix(c("A","B","B","C","B","D","C","E","D","E"),ncol=2,byrow=TRUE,dimnames=list(NULL,c("FROM","TO")))
+arc.set=matrix(c("A","B","B","C","B","D","C","E","D","E"),ncol=2,byrow=TRUE,
+               dimnames=list(NULL,c("FROM","TO")))
 e = gr
 arcs(e)=arc.set 
 
 par(mfrow=c(1,2)) 
 plot(gr)
 plot(e)
+#this is a comment
